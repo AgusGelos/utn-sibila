3c17b0144049cfbc0af144aa198493c795187c8a
BeFede <fedebutn@gmail.com>
2019-02-25 17:12:28 -0300

    Agrego POST, PUT y DELETE al concepto y defino interfaz para las equivalencias

--- software/java/src/main/java/web/ConceptoController.java
+++ software/java/src/main/java/web/ConceptoController.java
@@ -80,4 +80,104 @@ public class ConceptoController {
 
     }
 
+
+    @RequestMapping(path="/concepto/{nombre}", method=RequestMethod.PUT)
+    public ResponseEntity<String> updateConcepto(@PathVariable("nombre") String nombre, @RequestParam("nuevoNombre") String nuevoNombre){
+        ConceptManager cm = new ConceptManager ("remote:localhost/PPR","admin","admin");
+
+        Concepto concepto = cm.getConceptoByName(nombre);
+        String mensaje = "";
+        HttpStatus status;
+
+        if (concepto != null){
+
+            cm.updNombreConcepto(concepto, nuevoNombre);
+            status = HttpStatus.OK;
+        }
+        else {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            mensaje = "El concepto que se quiere actualizar no existe";
+        }
+
+
+        return new ResponseEntity<String>(mensaje, status);
+
+    }
+
+
+    @RequestMapping(path="/concepto/{nombre}", method=RequestMethod.DELETE)
+    public ResponseEntity<String> deleteConcepto(@PathVariable("nombre") String nombre){
+        ConceptManager cm = new ConceptManager ("remote:localhost/PPR","admin","admin");
+
+        Concepto concepto = cm.getConceptoByName(nombre);
+        String mensaje = "";
+        HttpStatus status;
+
+        if (concepto != null){
+
+            Boolean borrado = cm.delConceptoSafe(concepto);
+
+            if (borrado){
+                status = HttpStatus.OK;
+                mensaje = "Se ha borrado el concepto con éxito";
+            }
+            else {
+                status = HttpStatus.INTERNAL_SERVER_ERROR;
+                mensaje = "Existen relaciones asociadas al concepto. No es posible borrarlo";
+            }
+
+        }
+        else {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            mensaje = "El concepto que se quiere actualizar no existe";
+        }
+
+
+        return new ResponseEntity<String>(mensaje, status);
+
+    }
+
+
+    @RequestMapping(path="/concepto/{nombreConcepto}/equivalencias", method=RequestMethod.GET)
+    public ResponseEntity<HashMap> getEquivalenciasByConcepto(@PathVariable("nombreConcepto") String nombre){
+        ConceptManager cm = new ConceptManager ("remote:localhost/PPR","admin","admin");
+
+        Concepto concepto = cm.getConceptoByName(nombre);
+        HashMap<String, Object> equivalencias = new HashMap();
+
+        String mensaje = "";
+        HttpStatus status;
+
+        if (concepto != null){
+            equivalencias.put("concepto", concepto.getNombre());
+            equivalencias.put("equivalencias", cm.getEquivalencias(concepto.getNombre()));
+            status = HttpStatus.OK;
+        }
+        else {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        }
+
+
+        return new ResponseEntity<HashMap>(equivalencias, status);
+
+    }
+
+
+
+    @RequestMapping(path="/concepto/{nombreConcepto}/equivalencias/{nombreEquivalencia}", method=RequestMethod.GET)
+    public ResponseEntity<HashMap> getEquivalenciasByName(@PathVariable("nombreConcepto") String nombreConcepto, @PathVariable("nombreEquivalencia") String nombreEquivalencia){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @RequestMapping(path="/equivalencia/{nombre}", method=RequestMethod.GET)
+    public ResponseEntity<HashMap> getConceptoByEquivalencia(@PathVariable("nombre") String nombre){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+
+    @RequestMapping(path="/conceptos/{nombreConcepto}/relaciones/{tipoRelacion}", method=RequestMethod.GET)
+    public ResponseEntity<HashMap> countReferences(@PathVariable("nombreConcepto") String nombreConcepto, @PathVariable("tipoRelacion") String tipoRelacion){
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
 }
