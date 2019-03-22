d8e6ea786bce508fd67f324241768a7453905066
BeFede <fedebutn@gmail.com>
2019-03-22 19:35:56 -0300

    API para la corrección ortográfica y la clasificación de los términos de la misma

--- software/java/src/main/java/web/RespuestaController.java
+++ software/java/src/main/java/web/RespuestaController.java
@@ -1,6 +1,7 @@
 package web;
 
 import api.Respuesta;
+import api.Termino;
 import conceptmanager.ConceptManager;
 
 import controlador.Controller;
@@ -8,9 +9,13 @@ import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ortografia.ManejoLang;
 import ortografia.OrtographyManager;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 
 @RestController
@@ -34,6 +39,80 @@ public class RespuestaController {
 
     }
 
+
+    @RequestMapping(path="/respuesta/corregir", method=RequestMethod.POST)
+    public ResponseEntity corregirRespuesta(@RequestParam("respuesta") String respuestaText)  {
+
+        ConceptManager cm = new ConceptManager ("remote:localhost/PPR","admin","admin");
+        OrtographyManager ortografia = new OrtographyManager(cm);
+        ManejoLang ml = new ManejoLang();
+        HttpStatus status = HttpStatus.OK;
+
+        HashMap<String, Object> respuestaMap = new HashMap<>();
+
+        Respuesta respuesta = new Respuesta(respuestaText);
+
+
+
+        try{
+            respuesta = ortografia.corregirRespuesta(respuesta);
+            respuesta = cm.evaluarRespuesta(respuesta);
+
+            ArrayList<Termino> terminos = respuesta.getTerminos();
+            ArrayList<HashMap<String, Object>> terminosMap = new ArrayList<HashMap<String, Object>>();
+
+            for (Termino termino : terminos) {
+                HashMap<String, Object> terminoMap = new HashMap<String, Object>();
+
+                HashMap<String, Object> errorMap = new HashMap<String, Object>();
+
+                //LisugerenciasErrorMap = new HashMap<String>();
+
+                List<String> sugerencias = termino.getSugerenciasCorreccion();
+
+                /*for (String sugerencia : sugerencias) {
+                    sugerenciasErrorMap.put(sugerencia);
+                } */
+
+                errorMap.put("error", termino.hasErrors());
+                errorMap.put("sugerencias", sugerencias);
+
+                terminoMap.put("nombre",termino.getNombre());
+                terminoMap.put("error", errorMap);
+
+                String tipo = termino.getTipo();
+                terminoMap.put("tipo", tipo);
+
+                String sugerenciaTipo = tipo;
+                if (sugerenciaTipo == ""){
+                    sugerenciaTipo = ml.sugerenciaTipoConcepto(termino);
+                }
+                terminoMap.put("sugerenciaTipo", sugerenciaTipo);
+
+
+
+
+
+
+
+                terminosMap.add(terminoMap);
+            }
+
+
+            respuestaMap.put("terminos", terminosMap);
+
+            if (respuesta.hasErrors()){
+                status = HttpStatus.INTERNAL_SERVER_ERROR;
+            }
+        }
+        catch(IOException e){
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+
+        return new ResponseEntity<HashMap<String, Object>>(respuestaMap, status);
+
+    }
 
 
 }
